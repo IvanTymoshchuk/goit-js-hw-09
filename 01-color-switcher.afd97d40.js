91feab0a2cef3e3bda753b146fa220b169fae7de
IvanTymoshchuk <timohin2231@gmail.com>
2023-03-07 18:22:30 +0000

    Deploying to gh-pages from @ IvanTymoshchuk/goit-js-hw-09@d1150bac04f457c7b47407f1b8ce6103871facd5 🚀

---
+++ 01-color-switcher.afd97d40.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;t.addEventListener("click",(function(){t.disabled=!0,n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1500)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.afd97d40.js.map
---
+++ 01-color-switcher.df04a3b2.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let n=null;t.addEventListener("click",(function(){t.disabled=!0,n=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1500)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.df04a3b2.js.map
