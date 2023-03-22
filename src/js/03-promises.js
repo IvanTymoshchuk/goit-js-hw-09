a18c64a357decd52fb5bcac45015340300d48459
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-03-22 17:49:54 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,51 @@
+/*
+Завдання 3 - генератор промісів
+HTML містить розмітку форми, в поля якої користувач буде вводити першу затримку в мілісекундах, крок збільшення затримки для кожного промісу після першого і кількість промісів, яку необхідно створити.
+
+<form class="form">
+  <label>
+    First delay (ms)
+    <input type="number" name="delay" required />
+  </label>
+  <label>
+    Delay step (ms)
+    <input type="number" name="step" required />
+  </label>
+  <label>
+    Amount
+    <input type="number" name="amount" required />
+  </label>
+  <button type="submit">Create promises</button>
+</form>
+
+Напиши скрипт, який на момент сабміту форми викликає функцію createPromise(position, delay) стільки разів, скільки ввели в поле amount. Під час кожного виклику передай їй номер промісу (position), що створюється, і затримку, враховуючи першу затримку (delay), введену користувачем, і крок (step).
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    // Fulfill
+  } else {
+    // Reject
+  }
+}
+
+Доповни код функції createPromise таким чином, щоб вона повертала один проміс, який виконується або відхиляється через delay часу. Значенням промісу повинен бути об'єкт, в якому будуть властивості position і delay зі значеннями однойменних параметрів. Використовуй початковий код функції для вибору того, що потрібно зробити з промісом - виконати або відхилити.
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
+Бібліотека повідомлень
+УВАГА
+Наступний функціонал не обов'язковий для здавання завдання, але буде хорошою додатковою практикою.
+
+Для відображення повідомлень користувачеві, замість console.log(), використовуй бібліотеку notiflix.
+*/
+
 import Notiflix from 'notiflix';
 
 const form = document.querySelector('.form');
